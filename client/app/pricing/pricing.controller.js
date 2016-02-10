ac3e0a334768bb5335f75ada597de8060c4d0d92
dekhangga <danggareska@yahoo.com>
2016-02-10 16:45:44 +0700

    updated logistic flat prices

--- client/app/pricing/pricing.controller.js
+++ client/app/pricing/pricing.controller.js
@@ -28,40 +28,224 @@ angular.module('adminApp')
         value: '2'
     },  ];
 
-    $scope.companies = [{
-        key: 'Master',
-        value: '0'
-    }]
-
     $scope.company = {
         key: 'Master',
         value: '0'
     };
+
+    $scope.companies = [$scope.company]
 
     $scope.pickup = {
         key: 'Same Day',
         value: '1'
     };
 
-    $scope.defaultPrices = [{
-        MaxWeight: 3,
-        MaxDimension1: 30,
-        MaxDimension2: 20,
-        MaxDimension3: 10,
-        Price: null
-    }, {
-        MaxWeight: 6,
-        MaxDimension1: 30,
-        MaxDimension2: 30,
-        MaxDimension3: 15,
-        Price: null
-    }, {
-        MaxWeight: 10,
-        MaxDimension1: 60,
-        MaxDimension2: 60,
-        MaxDimension3: 30,
-        Price: null
-    } ];
+    $scope.defaultPrices = {
+        bike: [],
+        van: [],
+        smalltruck: [],
+        mediumtruck: []
+    }
+
+    $scope.percentage = {
+        bike: {
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null
+        },
+        van: {
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null
+        },
+        smalltruck: {
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null
+        },
+        mediumtruck: {
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null
+        }
+    }
+
+    /**
+     * Change prices based on vehicle's Price, LogisticShare, OurShare or DriverShare
+     * 
+     * @return {void}
+     */
+    $scope.changePrices = function(vehicle) {
+        $scope.prices[vehicle].forEach(function(price) {
+            price.LogisticShare = price.Price * Number($scope.percentage[vehicle].LogisticShare) / 100
+            price.OurShare = price.Price * Number($scope.percentage[vehicle].OurShare) / 100
+            price.DriverShare = price.Price * Number($scope.percentage[vehicle].DriverShare) / 100
+        })
+        var sum = Number($scope.percentage[vehicle].LogisticShare) +
+            Number($scope.percentage[vehicle].OurShare) +
+            Number($scope.percentage[vehicle].DriverShare)
+        if (sum > 100) {
+            alert('Exceed 100%')
+            $scope.percentage[vehicle].LogisticShare = null
+            $scope.percentage[vehicle].OurShare = null
+            $scope.percentage[vehicle].DriverShare = null
+            return false
+        }
+    }
+
+    /**
+     * Get default prices for each vehicle
+     * 
+     * @return {void}
+     */
+    $scope.getDefaultPrices = function() {
+        $scope.defaultPrices.bike.push({
+            VehicleID: 1,
+            MaxWeight: 3,
+            MaxDimension1: 30,
+            MaxDimension2: 20,
+            MaxDimension3: 10,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.bike.push({
+            VehicleID: 1,
+            MaxWeight: 6,
+            MaxDimension1: 30,
+            MaxDimension2: 30,
+            MaxDimension3: 15,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.bike.push({
+            VehicleID: 1,
+            MaxWeight: 10,
+            MaxDimension1: 60,
+            MaxDimension2: 60,
+            MaxDimension3: 30,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.van.push({
+            VehicleID: 2,
+            MaxCBM: 1,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.van.push({
+            VehicleID: 2,
+            MaxCBM: 2,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.smalltruck.push({
+            VehicleID: 3,
+            MaxCBM: 3,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.smalltruck.push({
+            VehicleID: 3,
+            MaxCBM: 4,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.smalltruck.push({
+            VehicleID: 3,
+            MaxCBM: 5,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.mediumtruck.push({
+            VehicleID: 4,
+            MaxCBM: 7,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.mediumtruck.push({
+            VehicleID: 4,
+            MaxCBM: 8,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.mediumtruck.push({
+            VehicleID: 4,
+            MaxCBM: 10,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+        $scope.defaultPrices.mediumtruck.push({
+            VehicleID: 4,
+            MaxCBM: 11,
+            LogisticShare: null,
+            OurShare: null,
+            DriverShare: null,
+            Price: null
+        })
+    }
+    $scope.getDefaultPrices()    
+
+    /**
+     * Assign price with default prices
+     * 
+     * @return {void}
+     */
+    $scope.assignDefaultPrices = function() {        
+        if ($scope.prices.bike.length === 0) {
+            $scope.prices.bike = $scope.defaultPrices.bike
+        }
+        if ($scope.prices.van.length === 0) {
+            $scope.prices.van = $scope.defaultPrices.van
+        }
+        if ($scope.prices.smalltruck.length === 0) {
+            $scope.prices.smalltruck = $scope.defaultPrices.smalltruck
+        }
+        if ($scope.prices.mediumtruck.length === 0) {
+            $scope.prices.mediumtruck = $scope.defaultPrices.mediumtruck
+        }
+    }
+
+    /**
+     * Assign price with desired prices
+     * 
+     * @return {void}
+     */
+    $scope.assignNewPrices = function(price) {
+        if (price.VehicleID===1) {
+            $scope.prices.bike.push(price);
+        }
+        if (price.VehicleID===2) {
+            $scope.prices.van.push(price);
+        }
+        if (price.VehicleID===3) {
+            $scope.prices.smalltruck.push(price);
+        }
+        if (price.VehicleID===4) {
+            $scope.prices.mediumtruck.push(price);
+        }
+    }
 
     /**
      * Assign pickup to the chosen item
@@ -110,24 +294,21 @@ angular.module('adminApp')
      */
     $scope.getPrices = function() {
         $rootScope.$emit('startSpin');
-        $scope.prices = []
+        $scope.prices = {
+            bike: [],
+            van: [],
+            smalltruck: [],
+            mediumtruck: []
+        }
         var params = {
             CompanyDetailID: $scope.company.value,
             PickupType: $scope.pickup.value
         }
         Services.showLogisticPrices(params).$promise.then(function(data) {
-            if (data.prices.length === 3) {
-                $scope.prices = data.prices
-            } else {
-                var idx = 0;
-                $scope.defaultPrices.forEach(function(price) {
-                    if (data.prices[idx]) {
-                        price.Price = data.prices[idx].Price;
-                    }
-                    $scope.prices.push(price);
-                    idx++;
-                });
-            }
+            data.prices.forEach(function(price) {
+                $scope.assignNewPrices(price);
+            });
+            $scope.assignDefaultPrices();
             $rootScope.$emit('stopSpin');
         });
     }
@@ -138,7 +319,6 @@ angular.module('adminApp')
      * @return {void}
      */
     $scope.savePrices = function() {
-        console.log($scope.prices);
         var params = {
             CompanyDetailID: $scope.company.value,
             PickupType: $scope.pickup.value,
@@ -147,6 +327,7 @@ angular.module('adminApp')
         Services.savePrices(params).$promise.then(function(data) {
             alert("Save success");
             $scope.getPrices();
+            window.location = '/pricing';
         });
     }
 
