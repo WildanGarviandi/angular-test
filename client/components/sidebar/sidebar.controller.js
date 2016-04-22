0c11098dbf25ea1997375cac3f21e079980ba395
imadmk <mk.imaduddin@gmail.com>
2016-04-22 16:31:31 +0700

    - Add scrollbar - Expandable sidebar - Sub-menu in sidebar

--- client/components/sidebar/sidebar.controller.js
+++ client/components/sidebar/sidebar.controller.js
@@ -16,4 +16,22 @@ angular.module('adminApp')
         $scope.isActive = function(route) {
             return route === $location.path();
         };
+
+        $(document).ready(function() {
+            $('.expand-button').click(function() {
+                $('.content').toggleClass('isOpen');
+                $('.side-panel').toggleClass('isOpen');
+                $('#expand').toggleClass('fa-angle-double-right');
+                $("[dropdown-id]").addClass('hidden');
+                $("[dropdown-caret]").removeClass('fa-caret-up');
+            });
+            $('.have-sub').click(function () {
+                var data = $(this).data('dropdown');
+                $("[dropdown-id='" + data + "']").toggleClass('hidden');
+                $("[dropdown-caret='" + data + "']").toggleClass('fa-caret-up');
+                $('.content').addClass('isOpen');
+                $('.side-panel').addClass('isOpen');
+                $('#expand').removeClass('fa-angle-double-right');
+            });
+        });
     });
