99ab3956cfb8f577b5c3582fee7fd77226e87850
dekhangga <danggareska@yahoo.com>
2016-04-26 14:29:09 +0700

    refactor and updated confgi environment

--- client/config/env.js
+++ client/config/env.js
@@ -1,7 +1,31 @@
 'use strict';
 
+function env() {
+    var location = window.location.host;
+    var parts = location.split('.');
+    var app = parts[0];
+    var country = parts[parts.length - 1];
+    switch (app + '|' + country) {
+        case 'staging|com':
+            var url = 'http://staging.api2.etobee.com';
+            break;        
+        case 'admin2|com':
+            var url = 'http://api2.etobee.com';
+            break;    
+        case 'staging|ph':
+            var url = 'http://staging.api2.etobee.com';
+            break;    
+        case 'admin2|ph':
+            var url = 'http://api2.etobee.com.ph';
+            break;
+        default:
+            var url = 'http://localhost:3001';
+    } 
+    return  {
+        'url': url + '/v2/admin/' 
+    }
+}
+
 var app = angular.module('config', ['ui.router']);
-app.constant('config', {
-  "url": "http://localhost:3001/v2/admin/"
-})
+app.constant('config', (env)());
 
