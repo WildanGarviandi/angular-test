ff939dcb30e6dca1532001bb381850e8705ffefe
imadmk <mk.imaduddin@gmail.com>
2016-04-27 15:49:22 +0700

    Refactor many css and remove unnecessary file.

--- client/components/sidebar/sidebar.controller.js
+++ client/components/sidebar/sidebar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('adminApp')
-    .controller('SidebarCtrl', function($scope, $location, $rootScope, usSpinnerService) {
+    .controller('SidebarCtrl', function($scope, $location, $rootScope, usSpinnerService, localStorageService) {
 
         $rootScope.$on('startSpin', function() {
             usSpinnerService.spin('spinner-1');
@@ -17,12 +17,25 @@ angular.module('adminApp')
             return route === $location.path();
         };
 
+        var toggleExpand = function () {
+            $('.container-hubs').toggleClass('isOpen');
+            $('.side-panel').toggleClass('isOpen');
+            $('#expand').toggleClass('fa-angle-double-right');
+            $('.image-caret').toggleClass('hidden');
+        };
+
+        if (localStorageService.get('isExpanded') === true) {
+            toggleExpand();
+        }
+
         $(document).ready(function() {
             $('.expand-button').click(function() {
-                $('.content').toggleClass('isOpen');
-                $('.side-panel').toggleClass('isOpen');
-                $('#expand').toggleClass('fa-angle-double-right');
-                $('.header-caret').toggleClass('hidden');
+                toggleExpand();
+                if (localStorageService.get('isExpanded') === true) {
+                    localStorageService.set('isExpanded', false);
+                } else {
+                    localStorageService.set('isExpanded', true);
+                }
             });
             $('.have-sub').click(function () {
                 var data = $(this).data('dropdown');
