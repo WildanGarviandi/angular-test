753043e873ba705b7663715adac66b344ad2faf7
azbyluthfan <azby.luthfan@gmail.com>
2016-05-01 16:57:05 +0700

    Refactor sidebar

--- client/app/app.js
+++ client/app/app.js
@@ -101,9 +101,9 @@ angular.module('adminApp', [
         // Redirect to login if route requires auth and you're not logged in
         $rootScope.$on('$stateChangeStart', function (event, next) {
             Auth.isLoggedInAsync(function(loggedIn) {
-            if (next.authenticate && !loggedIn) {
-                $location.path('/login');
-            }
+                if (next.authenticate && !loggedIn) {
+                    $location.path('/login');
+                }
             });
         });
 
--- client/components/sidebar/sidebar.controller.js
+++ client/components/sidebar/sidebar.controller.js
@@ -3,11 +3,12 @@
 angular.module('adminApp')
     .controller('SidebarCtrl', function($scope, $location, $rootScope, usSpinnerService, localStorageService) {
 
+        $scope.currentPath = $location.path();
         $scope.menus = {
             pricing: {
                 submenus: {
                     logistic: { routes: ['/pricing/logistic']},
-                    ecommerce: { route: ['/pricing/ecommerce']},
+                    ecommerce: { routes: ['/pricing/ecommerce']},
                 }
             },
             location: {
@@ -16,11 +17,11 @@ angular.module('adminApp')
                     cities: { routes: ['/cities'] }
                 }
             },
-            trip: {
+            trips: {
                 routes: ['/trips']
             },
-            hubs: {
-                routes: ['/hubs']
+            hub: {
+                routes: ['/hub']
             },
             drivers: {
                 routes: ['/drivers']
@@ -36,26 +37,44 @@ angular.module('adminApp')
         function routeActive(routes) {
             var active = false;
             routes.forEach(function (route) {
-                if (route === $location.path()) { 
+                if (new RegExp($scope.currentPath, 'i').test(route)) {
                     active = true; 
                 }
             });
             return active;
         };
 
-        Object.keys($scope.menus).forEach(function(menu) {
-            if (menu.submenus) {
-                Object.keys(menu.submenus).forEach(function(submenu) {
-                    if (submenu.routes && routeActive(submenu.routes)) {
-                        submenu.active = true;
-                        menu.active = true;
+        $scope.refreshSidebar = function() {
+            Object.keys($scope.menus).forEach(function(menu) {
+                if ($scope.menus[menu].submenus) {
+                    $scope.menus[menu].active = false;
+                    $scope.menus[menu].expanded = false;
+                    
+                    Object.keys($scope.menus[menu].submenus).forEach(function(submenu) {
+                        
+                        $scope.menus[menu].submenus[submenu].active = false;
+                        if ($scope.menus[menu].submenus[submenu].routes 
+                            && routeActive($scope.menus[menu].submenus[submenu].routes)
+                        ) {
+                            $scope.menus[menu].submenus[submenu].active = true;
+                            $scope.menus[menu].active = true;
+                            $scope.menus[menu].expanded = true;
+                        }
+                    });
+                } else {
+                    $scope.menus[menu].active = false;
+                    if ($scope.menus[menu].routes && routeActive($scope.menus[menu].routes)) {
+                        $scope.menus[menu].active = true;
                     }
-                });
-            } else {
-                if (menu.routes && routeActive(menu.routes)) {
-                    menu.active = true;
                 }
-            }
+            });
+        }
+
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            $scope.currentPath = '/' + toState.url;
+            console.log($scope.currentPath);
+            console.log('sidebar');
+            $scope.refreshSidebar();
         });
 
         $scope.expanded = localStorageService.get('expanded');
@@ -72,14 +91,8 @@ angular.module('adminApp')
         }
 
         $scope.toggleExpandMenu = function(menu) {
-            $scope.menus[menu].active = !$scope.menus[menu].active;
+            $scope.menus[menu].expanded = !$scope.menus[menu].expanded;
         }
 
-        $(document).ready(function() {
-            // $('.have-sub').click(function () {
-            //     var data = $(this).data('dropdown');
-            //     $("[id-dropdown='" + data + "']").toggleClass('hidden');
-            //     $("[caret-dropdown='" + data + "']").toggleClass('fa-caret-up');
-            // });
-        });
+        $scope.refreshSidebar();
     });
