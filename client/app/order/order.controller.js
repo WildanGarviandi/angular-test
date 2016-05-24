7dd6b2aaebab36ef0d76593e085309947e7fa4a9
dekhangga <danggareska@yahoo.com>
2016-05-24 18:36:10 +0700

    added zipcode validation

--- client/app/order/order.controller.js
+++ client/app/order/order.controller.js
@@ -53,6 +53,7 @@ angular.module('adminApp')
     };
 
     $scope.currency = config.currency + " ";
+    $scope.zipLength = config.zipLength;
     $scope.isFirstSort = true;
 
     /**
@@ -313,8 +314,16 @@ angular.module('adminApp')
     $scope.updateAddress = function(address) {
         if (address === 'pickup') {
             var params = $scope.order.PickupAddress;
+            if (isNaN($scope.order.PickupAddress.ZipCode)) {
+                alert('Pickup zipcode is not valid');
+                return;
+            }
         } else if (address === 'dropoff') {
             var params = $scope.order.DropoffAddress;
+            if (isNaN($scope.order.DropoffAddress.ZipCode)) {
+                alert('Dropoff zipcode is not valid');
+                return;
+            }
         }
         $rootScope.$emit('startSpin');
         Services2.updateAddress({
