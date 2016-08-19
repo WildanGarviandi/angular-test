3e502bd20b07dac39033f2d224ee8062f5f185b2
dekhangga <danggareska@yahoo.com>
2016-08-19 13:42:36 +0700

    set toggle for order features

--- client/config/env.js
+++ client/config/env.js
@@ -4,11 +4,21 @@ var app = angular.module('config', ['ui.router', 'mainConfig']);
 
 function config() {
     var additionalConfig = {
-        
+        features: {
+            order: {
+                reassign_driver: true,
+                reassign_fleet: true,
+                set_price: true,
+                cancel: true,
+                mark_delivered: true,
+                bulk_mark_delivered: true,
+                return_sender: true,
+                copy_cancelled: true
+            }
+        }
     };
     var config = _.extend({}, mainConfig(), additionalConfig);
     return config;
 }
 
 app.constant('config', (config)());
-
