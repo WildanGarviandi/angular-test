6f93951419a0ccc6f0adce3512e398ad45ff179f
Evan Gozali <evangozali@yahoo.com>
2016-11-09 12:07:00 +0700

    update config for sandbox env DEVOPS-29

--- client/config/config.js
+++ client/config/config.js
@@ -16,6 +16,15 @@ function mainConfig() {
             var zipLength = 5;
             var activeMerchant = [0, 0, 0, 0, 0, 0];
             var countryCode = '+62';
+            break;   
+        case 'sandbox-v3|com':
+            var url = 'http://sandbox-v2.api2.etobee.com';
+            var defaultLocation = { Latitude: -6.2115, Longitude: 106.8452 };
+            var currency = 'Rp';
+            var decimalSeparator = ',';
+            var zipLength = 5;
+            var activeMerchant = [0, 0, 0, 0, 0, 0];
+            var countryCode = '+62';
             break;        
         case 'admin2|com':
             var url = 'https://api2.etobee.com';
