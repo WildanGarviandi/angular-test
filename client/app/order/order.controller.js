b8d70ca2b0649b86f8f8215839fd0a2a484c91d9
fadhil_ismail <fadhil.ismail4@gmail.com>
2017-01-24 12:20:46 +0700

    feature/ADMINDASH-358: cannot filter order type

--- client/app/order/order.controller.js
+++ client/app/order/order.controller.js
@@ -304,7 +304,7 @@ angular.module('adminApp')
         },
         'OrderType': {
             model: 'orderType',
-            pick: 'value',
+            pick: 'key',
             collection: 'orderTypes'
         },
         'IsAttempt': {
