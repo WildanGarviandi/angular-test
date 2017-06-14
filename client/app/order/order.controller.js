d4f72833efa210487094ae71b6a35fb12a4fa12b
fadhil_ismail <fadhil.ismail4@gmail.com>
2017-06-14 11:39:28 +0700

    limit multiple EDS Filter by 200 orders

--- client/app/order/order.controller.js
+++ client/app/order/order.controller.js
@@ -32,6 +32,7 @@ angular.module('adminApp')
 
     $scope.itemsByPage = $location.search().limit || 10;
     $scope.offset = $location.search().offset || 0;
+    $scope.limitMultipleEDSFilter = 200;
     $scope.isFirstLoaded = true;
     $scope.isFirstSort = true;
 
@@ -2036,6 +2037,9 @@ angular.module('adminApp')
     };
 
     $scope.filterMultipleEDS = function () {
+        if ($scope.userOrderNumbers.length > $scope.limitMultipleEDSFilter) {
+            return SweetAlert.swal('Error', 'filter maximum ' + $scope.limitMultipleEDSFilter + ' orders only', 'error');
+        }
         getExistOrder();
         var isFilterEDS = true;
         $scope.getOrder(isFilterEDS);
