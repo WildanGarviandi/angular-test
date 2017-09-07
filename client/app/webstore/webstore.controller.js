c90cc5eb3185705af83ffe0ccdfc69c586a30221
fadhil_ismail <fadhil.ismail4@gmail.com>
2017-09-07 15:38:50 +0700

    feature/ADMINDASH-519: Add data on webstore page

--- client/app/webstore/webstore.controller.js
+++ client/app/webstore/webstore.controller.js
@@ -525,13 +525,7 @@ angular.module('adminApp')
         $scope.offset = Math.ceil(value);
     }
 
-    /**
-     * Get all webstores
-     * 
-     * @return {void}
-     */
-    $scope.getWebstores = function() {
-        $rootScope.$emit('startSpin');
+    var getWebstoresParam = function () {
         var paramsQuery = {
             'name': 'name',
             'email': 'email',
@@ -551,16 +545,29 @@ angular.module('adminApp')
         });
 
         $location.search('offset', $scope.offset);
-        $scope.isLoading = true;
         var params = {
             name: $scope.searchFilter.name,
             email: $scope.searchFilter.email,
             address: $scope.searchFilter.address,
             status: $scope.statusFilter.key,
-            postPaidPayment: $scope.postPaidPaymentFilter.key,
+            postPaidPayment: ($scope.postPaidPaymentFilter.key !== 'all') ? $scope.postPaidPaymentFilter.key : '',
             offset: $scope.offset,
             limit: $scope.itemsByPage
         };
+
+        return params;
+    }
+
+    /**
+     * Get all webstores
+     * 
+     * @return {void}
+     */
+    $scope.getWebstores = function() {
+        $rootScope.$emit('startSpin');
+        $scope.isLoading = true;
+        
+        var params = getWebstoresParam();
         Webstores.getWebstore(params).$promise.then(function(data) {
             $scope.displayed = _.map(data.data.webstores, function(webstore) {
                 return _.assign({}, webstore.webstore, {
@@ -1023,6 +1030,7 @@ angular.module('adminApp')
             maxExport = data.data.count;
             var mandatoryUrl = 'exportType=' + type + '&' + 'maxExport=' + maxExport;
             var params = {};
+                params = getWebstoresParam();
                 params.userType = 5;
             $window.open('/export?' + mandatoryUrl + '&' + $httpParamSerializer(params));
         });
